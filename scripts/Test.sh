cd1bfe4e928f7d17e0dc0e33ce635d79e9d8b20c
IamAshutoshDubey <dubeyashu.88@gmail.com>
2022-03-25 12:01:46 +0100

    fake commit

--- scripts/Test.sh
+++ scripts/Test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git log --oneline $(git describe --abbrev=0 --tags --exclude '*-rollback'  $(git rev-list --tags --skip=1 --max-count=1))..$(git describe --abbrev=0 --tags --exclude '*-rollback')
+git log --oneline $(git describe --abbrev=0 --tags --exclude '*-rollback'  $(git rev-list --tags --skip=1 --max-count=1))..$(git describe --abbrev=0 --tags --exclude '*-rollback1')
 
 
